99d49efccf8bfc2c392bb6615d60a06720455c20
Gert van Valkenhoef <gert@gertvv.nl>
2025-04-11 20:27:59 +0200

    Improve performance of copy_set and copy_insert on Erlang (#5)
    Applying the same technique of #3 to copy_set and copy_insert yields a meaningful performance gain for small arrays

--- src/glearray_ffi.erl
+++ src/glearray_ffi.erl
@@ -16,6 +16,14 @@ get_or_default(Array, Index, Default) ->
     E -> E
   end.
 
-set(Array, Index, Value) -> setelement(Index + 1, Array, Value).
+set(Array, Index, Value) ->
+  case catch setelement(Index + 1, Array, Value) of
+    {'EXIT', _} -> {error,nil};
+    A -> {ok,A}
+  end.
 
-insert(Array, Index, Value) -> erlang:insert_element(Index + 1, Array, Value).
+insert(Array, Index, Value) ->
+  case catch erlang:insert_element(Index + 1, Array, Value) of
+    {'EXIT', _} -> {error,nil};
+    A -> {ok,A}
+  end.
